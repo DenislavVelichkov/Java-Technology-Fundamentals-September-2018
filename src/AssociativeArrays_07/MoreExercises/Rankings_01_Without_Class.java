80ef5fb5a332a30953b3898ee5ae2824c1596a0a
DenislavVelichkov <denislav.velichkov@gmail.com>
2018-12-02 20:36:07 +0200

    My work so far in Java

--- src/AssociativeArrays_07/MoreExercises/Rankings_01_Without_Class.java
+++ src/AssociativeArrays_07/MoreExercises/Rankings_01_Without_Class.java
@@ -9,16 +9,16 @@ public class Rankings_01_Without_Class {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String contestAndPassword = sc.nextLine();
+        String contestAndPass = sc.nextLine();
         Map<String, String> contestContainer = new LinkedHashMap<>();
         Map<String, Map<String, Integer>> userDB = new LinkedHashMap<>();
 
-        while (!contestAndPassword.equals("end of contests")) {
-            String[] tokens = contestAndPassword.split(":");
+        while (!contestAndPass.equals("end of contests")) {
+            String[] tokens = contestAndPass.split(":");
             contestContainer.putIfAbsent(tokens[0], "");
             contestContainer.put(tokens[0], tokens[1]);
 
-            contestAndPassword = sc.nextLine();
+            contestAndPass = sc.nextLine();
         }
 
         String submissions = sc.nextLine();
@@ -26,11 +26,13 @@ public class Rankings_01_Without_Class {
             String[] tokens = submissions.split("=>");
 
             if ((contestContainer.containsKey(tokens[0])) &&
-                    (tokens[1].equals(contestContainer.get(tokens[0])))) {
+                (tokens[1].equals(contestContainer.get(tokens[0])))) {
                 userDB.putIfAbsent(tokens[2], new LinkedHashMap<>());
                 userDB.get(tokens[2]).putIfAbsent(tokens[0], 0);
-                if (userDB.get(tokens[2]).get(tokens[0]) < Integer.parseInt(tokens[3])) {
-                    userDB.get(tokens[2]).put(tokens[0], Integer.parseInt(tokens[3]));
+                if (userDB.get(tokens[2]).get(tokens[0]) <
+                        Integer.parseInt(tokens[3])) {
+                    userDB.get(tokens[2]).put(tokens[0],
+                        Integer.parseInt(tokens[3]));
                 }
 
             }
@@ -59,12 +61,13 @@ public class Rankings_01_Without_Class {
                 .forEach(entry -> {
                     System.out.printf("%s%n", entry.getKey());
 
-                    entry.getValue().entrySet().stream().sorted((o1, o2) -> {
-                        int result = o2.getValue().compareTo(o1.getValue());
-                        return result != 0 ? result : 1;
-
+                    entry.getValue().entrySet().stream()
+                            .sorted((o1, o2) -> {
+                            int result = o2.getValue().compareTo(o1.getValue());
+                            return result != 0 ? result : 1;
                     }).forEach(v -> {
-                        System.out.printf("#  %s -> %d%n", v.getKey(), v.getValue());
+                        System.out.printf("#  %s -> %d%n",
+                                v.getKey(), v.getValue());
                     });
                 });
     }
