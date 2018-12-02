9b43c819aa1628870e8eb0f2f087582c269a1e67
DenislavVelichkov <denislav.velichkov@gmail.com>
2018-12-02 14:31:23 +0200

    My work so far in Java

---
+++ src/Exams/FinalDemoExam_01_12/Dictionary_01.java
@@ -0,0 +1,57 @@
+package Exams.FinalDemoExam_01_12;
+
+import java.util.*;
+
+public class Dictionary_01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Map<String, List<String>> dictionary = new LinkedHashMap<>();
+        List<String> listOfWords = new ArrayList<>();
+
+        String input = sc.nextLine();
+        while (true) {
+            if (input.equals("End")) {
+
+                dictionary.entrySet().stream()
+                        .sorted(Map.Entry.comparingByKey())
+                        .forEach(k -> {
+                            if (listOfWords.contains(k.getKey())) {
+                                System.out.println(k.getKey());
+                                k.getValue().stream().sorted((o1, o2) -> {
+                                    int resul = Integer.compare(o2.length(), o1.length());
+                                    return resul != 0 ? resul : 1;
+                                }).forEach(v -> System.out.printf(" -%s%n", v));
+                            }
+                        });
+
+                break;
+            }
+
+            if (input.equals("List")) {
+                dictionary.entrySet().stream()
+                        .sorted(Map.Entry.comparingByKey())
+                        .forEach(e -> System.out.printf("%s ", e.getKey()));
+
+                break;
+            }
+
+            String[] words = input.split(" \\| ");
+
+            if (words[0].contains(": ")) {
+
+                for (int i = 0; i < words.length; i++) {
+                    String[] tokens = words[i].split(": ");
+                    dictionary.putIfAbsent(tokens[0], new ArrayList());
+                    dictionary.get(tokens[0]).add(tokens[1]);
+                }
+
+            } else {
+                listOfWords.addAll(Arrays.asList(words));
+            }
+
+            input = sc.nextLine();
+        }
+    }
+}
