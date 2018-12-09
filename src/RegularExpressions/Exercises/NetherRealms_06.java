b429cb2531e68779fd9685a177b77b0e6402b10a
Denzo <denislav.velichkov@gmail.com>
2018-12-09 22:49:53 +0200

    Updates!

--- src/RegularExpressions/Exercises/NetherRealms_06.java
+++ src/RegularExpressions/Exercises/NetherRealms_06.java
@@ -1,60 +1,65 @@
 package RegularExpressions.Exercises;
 
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.util.*;
+        import java.util.regex.Matcher;
+        import java.util.regex.Pattern;
 
 public class NetherRealms_06 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = sc.nextLine();
-        Map<String, Integer> demonsAndHealth = new TreeMap<>();
-        Map<String, Double> demonsAndDmg = new TreeMap<>();
-        String[] diffDemons = input.split("[,\\s]+");
+        String input = reader.readLine();
+        Map<String, Double> demons = new LinkedHashMap<>();
+        Map<String, Double> demonsDamage = new HashMap<>();
+        String[] eachDemon = input.split("[,\\s]+");
 
-        for (int i = 0; i < diffDemons.length; i++) {
-            String demonName = diffDemons[i];
+        if (input.isEmpty()) return;
 
-            if (checkName(diffDemons[i])) {
-                demonsAndHealth.putIfAbsent(demonName, 0);
-                demonsAndDmg.putIfAbsent(demonName, 0.0);
+        for (String demonName : eachDemon) {
+                demons.putIfAbsent(demonName, 0d);
+                demonsDamage.putIfAbsent(demonName, 0d);
 
-                int health = addDemonHealth(diffDemons[i]);
-                double dmg = addDamage(diffDemons[i]);
+                double health = addDemonHealth(demonName);
+                double dmg = addDamage(demonName);
 
-                demonsAndHealth.put(demonName, health);
-                demonsAndDmg.put(demonName, dmg);
-            }
-
+                demons.put(demonName, health);
+                demonsDamage.put(demonName, dmg);
         }
 
-        demonsAndHealth.forEach((s, integer) -> {
-            System.out.printf("%s - %d health, %.2f damage%n", s, integer, demonsAndDmg.get(s));
-        });
+        demons
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .forEach(value -> {
+                    double dmg = demonsDamage.get(value.getKey());
+                    System.out.printf("%s - %.0f health, %.2f damage%n",
+                            value.getKey(), value.getValue(), dmg);
+                        });
+
     }
 
-    private static double addDamage(String diffDemon) {
-        double sum = 0.0;
-        String[] tokensDigits = diffDemon.split("[^0-9-+\\.]+");
-        String[] tokensMultOrDevide = diffDemon.split("[^*\\\\]+");
+    private static double addDamage(String demon) {
+        double sum = 0d;
+        Pattern findDmg = Pattern.compile("[-+.0-9]+");
+        String[] arithmetic = demon.split("[^/*]+");
 
-        for (int i = 0; i < tokensDigits.length; i++) {
-            if (!tokensDigits[i].isEmpty()) {
-                sum += Double.parseDouble(tokensDigits[i]);
-            }
+        Matcher digits = findDmg.matcher(demon);
+        while (digits.find()) {
+            double digit = Double.parseDouble(digits.group());
+            sum += digit;
         }
 
-        for (int i = 0; i < tokensMultOrDevide.length; i++) {
-            if (!tokensMultOrDevide[i].isEmpty()) {
-                for (int j = 0; j < tokensMultOrDevide[i].length(); j++) {
-                    String charSeq = tokensMultOrDevide[i];
-                    if (charSeq.charAt(j) == '*') {
-                        sum *= 2;
-                    } else {
-                        sum /= 2;
-                    }
+        for (String arithmeticSignCluster : arithmetic) {
+            char[] arithmeticSigns = arithmeticSignCluster.toCharArray();
+            for (char sign : arithmeticSigns) {
+                if (sign == '*') {
+                    sum *= 2;
+                } else if (sign == '/') {
+                    sum /= 2;
                 }
             }
         }
@@ -62,28 +67,16 @@ public class NetherRealms_06 {
         return sum;
     }
 
-    private static int addDemonHealth(String diffDemon) {
-        int sum = 0;
+    private static double addDemonHealth(String demon) {
+        double sum = 0d;
+        Pattern pattern = Pattern.compile("[^0-9+\\-*/.]");
+        Matcher matcher = pattern.matcher(demon);
 
-        for (int i = 0; i < diffDemon.length(); i++) {
-            if (Character.isLetter(diffDemon.charAt(i))) {
-                sum += diffDemon.charAt(i);
-            }
+        while (matcher.find()) {
+            char match = matcher.group().charAt(0);
+            sum += match;
         }
 
         return sum;
-    }
-
-    private static boolean checkName(String input) {
-        Pattern pattern = Pattern.compile("[A-Za-z]+");
-        Matcher matcher = pattern.matcher(input);
-
-        while (matcher.find()) {
-            if (matcher.group().matches("[A-Za-z]+")) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
