6be0d2d656bbf4440274542832053c847ce9a472
DenislavVelichkov <denisla.velichkov@gmail.com>
2018-12-16 19:47:43 +0200

    Updates!

--- src/Exams/FinalExam161218/SongEncryption.java
+++ src/Exams/FinalExam161218/SongEncryption.java
@@ -12,77 +12,55 @@ public class SongEncryption {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String line = reader.readLine();
-        int keyLength = 0;
+        int keyLength;
 
-        while (!line.equals("End")) {
-            Pattern pattern = Pattern.compile("[A-Z][a-z]+\\s?[A-Z]?[a-z]+\\'?[a-z]+:[A-Z]+\\s?[A-Z]+\\s?[A-Z]+\\s?");
+        while (!line.equals("end")) {
+            Pattern pattern = Pattern.compile("^([A-Z])[a-z]+\\s*([a-z'\\s]*):([A-Z]+\\s)+");
+            /*([A-Z])[a-z]+\s*([a-z'\s]*):([A-Z]+\s)+*/
+            /*(([A-Z]){1}[a-z]+)\s*([a-z'\s]*):([A-Z]+\s)+*/
             Matcher matcher = pattern.matcher(line);
             if (matcher.find()) {
                 String[] tokens = line.split(":");
                 String artist = tokens[0];
                 String song = tokens[1];
+                keyLength = artist.length();
 
-
-                String finaleResult = encryptArtist(artist, keyLength) + "@" + encryptSong(song, keyLength);
+                String finaleResult =
+                        encrypt(artist, keyLength) +
+                        "@" +
+                        encrypt(song, keyLength);
 
                 System.out.printf("Successful encryption: %s%n", finaleResult);
             } else {
                 System.out.println("Invalid input!");
-                line = reader.readLine();
-                continue;
             }
 
             line = reader.readLine();
         }
     }
 
-    private static String encryptSong(String song, int length) {
-//        List<Character> tempList = new ArrayList<>();
+    private static String encrypt(String text, int length) {
         StringBuilder str = new StringBuilder();
+        char[] tempCharArray = text.toCharArray();
 
-        for (int i = 0; i < song.length(); i++) {
-            if (song.charAt(i) == 32 || song.charAt(i) == 44) {
-                continue;
-            }
-            if (length + song.charAt(i) <= 90 || length + song.charAt(i) <= 122) {
-                int temp = song.charAt(i) + length;
+        for (int i = 0; i < tempCharArray.length; i++) {
+            if (length + tempCharArray[i] <= 90) {
+                int temp = tempCharArray[i] + length;
                 str.append((char) temp);
             } else {
-                if (length + song.charAt(i) > 122) {
-                    int tempLenth = ((length + song.charAt(i)) - 122) + 97;
-                    str.append((char) tempLenth);
-                }
-                if (length + song.charAt(i) > 90) {
-                    int tempLenth = ((length + song.charAt(i)) - 90) + 65;
-                    str.append((char) tempLenth);
-                }
+                int tempLenth = ((length + text.charAt(i)) - 90) + 65;
+                str.append((char) tempLenth);
+            }
+
+            if (length + tempCharArray[i] <= 122) {
+                int temp = tempCharArray[i] + length;
+                str.append((char) temp);
+            } else {
+                int tempLength = ((length + text.charAt(i)) - 122) + 97;
+                str.append((char) tempLength);
             }
         }
 
-        return str.toString();
-    }
-
-    private static String encryptArtist(String artist, int length) {
-//        List<Character> tempList = new ArrayList<>();
-        StringBuilder str = new StringBuilder();
-
-        for (int i = 0; i < artist.length(); i++) {
-            if (length + artist.charAt(i) <= 90 || length + artist.charAt(i) <= 122) {
-                int temp = artist.charAt(i) + length;
-                str.append((char) temp);
-            } else {
-                if (length + artist.charAt(i) > 122) {
-                    int tempLenth = ((length + artist.charAt(i)) - 122) + 97;
-                    str.append((char) tempLenth);
-
-                }
-                if (length + artist.charAt(i) > 90) {
-                    int tempLenth = ((length + artist.charAt(i)) - 90) + 65;
-                    str.append((char) tempLenth);
-
-                }
-            }
-        }
         return str.toString();
     }
 }
