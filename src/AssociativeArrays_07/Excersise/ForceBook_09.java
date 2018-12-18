b3ac30c5ec943bc6d054961fef3f970a4c84cec9
Denzo <denislav.velichkov@gmail.com>
2018-12-18 19:19:58 +0200

    Updates!

--- src/AssociativeArrays_07/Excersise/ForceBook_09.java
+++ src/AssociativeArrays_07/Excersise/ForceBook_09.java
@@ -1,24 +1,28 @@
 package AssociativeArrays_07.Excersise;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class ForceBook_09 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String line = sc.nextLine();
+
+        String line = reader.readLine();
         String forceUser = "";
         String forceSide = "";
 
-        Map<String, ArrayList<String>> journey = new HashMap<>();
+        Map<String, ArrayList<String>> journey = new LinkedHashMap<>();
 
         while (!line.equals("Lumpawaroo")) {
             String[] tokens;
             boolean wasThisCurrentCmd = false;
 
-            if (line.contains(" | ")) {
+            if (line.contains("|")) {
                 tokens = line.split(" \\| ");
                 forceUser = tokens[1];
                 forceSide = tokens[0];
@@ -29,37 +33,53 @@ public class ForceBook_09 {
                 wasThisCurrentCmd = true;
             }
 
+            boolean doesHeAExist = false;
             if (!journey.containsKey(forceSide)) {
                 journey.put(forceSide, new ArrayList<>());
             }
 
-            if (!journey.get(forceSide).contains(forceUser)) {
-                journey.get(forceSide).add(forceUser);
-            }
-
             if (wasThisCurrentCmd) {
                 for (Map.Entry<String, ArrayList<String>> entry : journey.entrySet()) {
-                    entry.getValue().remove(forceUser);
+                    if (entry.getValue().remove(forceUser)) {
+                        break;
+                    }
                     //check and remove entry from Map
                 }
 
-                if (!journey.get(forceSide).contains(forceUser)) {
-                    journey.get(forceSide).add(forceUser);
-                    System.out.printf("%s joins the %s side!%n", forceUser, forceSide);
+                journey.get(forceSide).add(forceUser);
+                System.out.printf("%s joins the %s side!%n", forceUser, forceSide);
+            }
+
+            for (Map.Entry<String, ArrayList<String>> entr : journey.entrySet()) {
+                if (entr.getValue().contains(forceUser)) {
+                    doesHeAExist = true;
+                    break;
                 }
             }
 
-            line = sc.nextLine();
+            if (doesHeAExist) {
+                line = reader.readLine();
+                continue;
+            }else {
+                journey.get(forceSide).add(forceUser);
+            }
+
+
+            line = reader.readLine();
         }
 
         journey.entrySet().stream()
-                .sorted(Map.Entry.<String, ArrayList<String>>comparingByValue(Comparator.comparingInt(ArrayList::size)).reversed()
-                        .thenComparing(Map.Entry.comparingByKey()))
+                .sorted((o1, o2) -> {
+                    int result = Integer.compare(o2.getValue().size(), o1.getValue().size());
+                    return result != 0 ? result : o1.getKey().compareTo(o2.getKey());
+                })
                 .forEach(e -> {
-                    Collections.sort(e.getValue());
                     if (e.getValue().size() > 0) {
                         System.out.printf("Side: %s, Members: %d%n", e.getKey(), e.getValue().size());
-                        e.getValue().forEach(val -> System.out.printf("! %s%n", val));
+                        e.getValue()
+                                .stream()
+                                .sorted(String::compareTo)
+                                .forEach(val -> System.out.printf("! %s%n", val));
                     }
                 });
     }
