e062082ffb672e5548dda61cfefc9d56c86816cf
Denzo <denislav.velichkov@gmail.com>
2019-01-24 17:00:04 +0200

    Updates!

--- src/AssociativeArrays_07/Excersise/LegendaryFarming_03.java
+++ src/AssociativeArrays_07/Excersise/LegendaryFarming_03.java
@@ -13,9 +13,9 @@ public class LegendaryFarming_03 {
         materialsCount.put("fragments", 0);
         materialsCount.put("motes", 0);
         materialsCount.put("shards", 0);
-        boolean isObtrained = false;
+        boolean isObtained = false;
 
-        while (!isObtrained) {
+        while (!isObtained) {
             String[] materials = sc.nextLine().split(" ");
 
             for (int i = 0; i < materials.length - 1; i += 2) {
@@ -37,7 +37,7 @@ public class LegendaryFarming_03 {
                             System.out.println("Valanyr obtained!");
                         }
 
-                        isObtrained = true;
+                        isObtained = true;
                         break;
                     }
 
@@ -47,23 +47,12 @@ public class LegendaryFarming_03 {
                 }
             }
         }
-//First way of sorting hashmaps.
+
         materialsCount.entrySet().stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue()
                 .reversed()
                 .thenComparing(Map.Entry.comparingByKey()))
                 .forEach(e -> System.out.printf("%s: %d%n", e.getKey(), e.getValue()));
-//Second way of sorting hashmaps, Alphabetical ascending order. For descending order reverse (k, v) values
-        /*materialsCount.entrySet().stream()
-                .sorted((k, v) -> {
-                    if (Integer.compare(k.getValue(), v.getValue()) == 0) {
-                        return k.getKey().compareTo(v.getKey());
-                    } else {
-                        return v.getValue() - k.getValue();
-                    }
-                })*/
-
-
 
         junk.forEach((k, v) -> System.out.printf("%s: %d%n", k, v));
     }
